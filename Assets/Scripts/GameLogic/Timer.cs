0b1b09d744c03dddde8db28ec4d407c525ae5a3d
Barry <barry.p.evans@gmail.com>
2016-12-14 09:57:16 -0800

    [*] wheels added to car model [+] timer and count down in progress

---
+++ Assets/Scripts/GameLogic/Timer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    bool running = false;
+    float startTime = 0;
+    public void StartTimer()
+    {
+        startTime = Time.time;
+        running = true;
+    }
+   
+
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time- startTime;
+        int mins =(int)(t / 60);
+        int seconds = ((int)t  -mins*60);
+        int milis =(int)((t - (int)t) * 100);
+        string milisstring = milis < 10 ? "0" + milis : ""+milis;
+       GetComponent<Text>().text =mins+"."+ seconds + "." + milisstring;
+	}
+}
