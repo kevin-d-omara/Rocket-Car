c58e69f233d65c21156aefa1eba1d802ea873a2d
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-12-18 15:05:39 -0800

    part of PauseController; base level for Iyac

---
+++ Assets/Scripts/GameLogic/PauseController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PauseController : MonoBehaviour
+{
+    public List<KeyCode> pauseKeys;
+
+    private bool isPaused = false;
+
+    private GameObject menuButton;
+
+    private void Awake()
+    {
+        menuButton = transform.Find("Menu").gameObject;
+        menuButton.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        //GameManager.OnGameOver += PauseGame;
+    }
+
+    private void OnDisable()
+    {
+        //GameManager.OnGameOver -= PauseGame;
+    }
+
+    private void Update()
+    {
+        if (isPaused)
+        {
+            foreach (KeyCode key in pauseKeys)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    ResumeGame();
+                    menuButton.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            foreach (KeyCode key in pauseKeys)
+            {
+                if (Input.GetKeyDown(key))
+                {
+                    PauseGame();
+                    menuButton.SetActive(true);
+                }
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    private void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void OnDestroy()
+    {
+        ResumeGame();
+    }
+}
