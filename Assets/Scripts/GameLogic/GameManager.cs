c4435038d6a6e731f6786425aa05cd25822f423a
Kevin O'Mara <kevin.d.omara@gmail.com>
2016-12-18 22:18:42 -0800

    GameType selectable in RequiredObjects>GameManaber>GameType: Laps or RaceToFinish

--- Assets/Scripts/GameLogic/GameManager.cs
+++ Assets/Scripts/GameLogic/GameManager.cs
@@ -9,12 +9,15 @@ public class GameManager : MonoBehaviour
     public int CurrentLap { get; private set; }
     public int NumberOfRetries { get; private set; }
 
+    public delegate void RaceVictory();
+    public static event RaceVictory OnRaceVictory;
+
     [SerializeField] private GameObject mainCamera; // Main Camera instance
     [SerializeField] private GameObject spawnPoint; // Checkpoint instance
     [SerializeField] private GameObject endPoint;   // Checkpoint instance
     [SerializeField] private GameObject rocketCarPrefab;
     [SerializeField] private GameTypes gameType;
-    [SerializeField] private int laps; // ignore if GameType is RaceToFinish
+    [SerializeField] private int laps = 1; // ignore if GameType is RaceToFinish
 
     private GameObject lastCheckpoint;
     private GameObject rocketCar;
@@ -24,7 +27,7 @@ public class GameManager : MonoBehaviour
     {
         lastCheckpoint = spawnPoint;
         PlayerTime = 0f;
-        CurrentLap = 0;
+        CurrentLap = 1;
         NumberOfRetries = 0;
         SpawnCar();
     }
@@ -58,6 +61,56 @@ public class GameManager : MonoBehaviour
     {
         lastCheckpoint = checkpoint;
         rocketCar.GetComponentInChildren<ThrusterController>().ResetFuel();
+
+        if (gameType == GameTypes.RaceToFinish && checkpoint == endPoint)
+        {
+            Victory();
+        }
+        else if (gameType == GameTypes.Laps)
+        {
+            if (AllCheckpointsReached())
+            {
+                if (checkpoint == spawnPoint)
+                {
+                    if (CurrentLap == laps) // Victory!
+                    {
+                        Victory();
+                    }
+                    else
+                    {
+                        CurrentLap++;
+                        ResetAllCheckpoints();
+                    }
+                }
+                else
+                {
+                    spawnPoint.GetComponent<CheckpointController>().hasBeenReached = false;
+                }
+            }
+        }
+    }
+
+    private bool AllCheckpointsReached()
+    {
+        int numReached = 0;
+        foreach (GameObject checkpoint in checkpoints)
+        {
+            if (checkpoint.GetComponent<CheckpointController>().hasBeenReached)
+            {
+                numReached++;
+            }
+        }
+
+        return numReached == checkpoints.Length;
+    }
+
+    private void ResetAllCheckpoints()
+    {
+        foreach (GameObject checkpoint in checkpoints)
+        {
+            checkpoint.GetComponent<CheckpointController>().hasBeenReached = false;
+        }
+        spawnPoint.GetComponent<CheckpointController>().hasBeenReached = true;
     }
 
     private void SpawnCar()
@@ -80,5 +133,14 @@ public class GameManager : MonoBehaviour
     {
         Destroy(rocketCar);
         SpawnCar();
+    }
+
+    private void Victory()
+    {
+        Debug.Log("Victory!");
+        if (OnRaceVictory != null)
+        {
+            OnRaceVictory();
+        }
     }
 }
